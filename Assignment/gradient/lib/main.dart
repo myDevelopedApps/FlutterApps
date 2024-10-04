504d52d2eea4a05ddf42e3b8f0b5903f1fa71361
sunilbandichode2002 <sunilbandichode2002@gmail.com>
2024-10-04 16:02:37 +0530

    gradient

---
+++ Assignment/gradient/lib/main.dart
@@ -0,0 +1,40 @@
+import "package:flutter/material.dart";
+
+void main(){
+  runApp(const MainApp());
+}
+class MainApp extends StatelessWidget{
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("gradiant",
+          style: TextStyle(fontSize: 20,fontWeight:FontWeight.w500
+          ),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.blue,
+        ),
+        body: Center(
+          child: Container(
+            height: 200,
+            width: 200,
+            alignment: Alignment.bottomLeft,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors:[
+                Colors.black,
+                Colors.red
+                              ])
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
