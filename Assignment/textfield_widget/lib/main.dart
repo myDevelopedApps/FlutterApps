e2fe4ef20257e429ab4db9b5bf5b6a9f8eaff374
sunilbandichode2002 <sunilbandichode2002@gmail.com>
2024-10-10 22:46:47 +0530

    textFeild

--- Assignment/textfield_widget/lib/main.dart
+++ Assignment/textfield_widget/lib/main.dart
@@ -11,6 +11,8 @@ class MainApp extends StatefulWidget {
   State<StatefulWidget> createState()=> _MainApp();  
 }
 class _MainApp extends State{
+  TextEditingController nameController=TextEditingController();
+  String? myName;
 
   @override
   Widget build(BuildContext context) {
@@ -26,22 +28,68 @@ class _MainApp extends State{
               centerTitle: true,
               backgroundColor: Colors.blue,
             ),
-            body: const Column(
+            body:  Column(
               children: [
-                const SizedBox(
+                 const SizedBox(
                   height: 20,
                 ),
                 Padding(
-                  padding: const EdgeInsets.all(20),
+                  padding:  const EdgeInsets.all(20),
                   child: TextField(
-                    controller: nameController,
+                    controller:nameController,
                     style: const TextStyle(
                       fontSize: 20,
                     ),
-                    
-
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      suffixIcon: Icon(Icons.visibility),
+                      hintText: "Enter Name",
+                      hintStyle: TextStyle(
+                        fontSize: 25,
+                        color: Colors.grey,
+                      ),
+                      border: OutlineInputBorder(),
+                    ),
+                    onChanged: (String val){
+                      print("Val: $val");
+                    },
+                    onEditingComplete: (){
+                      print("Editing Completed");
+                    },
+                    onSubmitted: (value){
+                      print("Value Submitted : $value");
+                    },
                   ),
                 ),
+                const SizedBox(
+                  height: 20,
+                ),
+                GestureDetector(
+                  onTap: (){
+                    print("Add Data");
+                    myName=nameController.text;
+                    print("My Name:$myName");
+                    nameController.clear();
+                    setState(() {});
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 15,
+                      vertical: 5
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(15)
+                    ),
+                    child: const Text(
+                      "Add Data",style: 
+                      TextStyle(
+                        fontSize: 15,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                )
 
               ],
             ),
